253111d13391ad1e2684c543cb38635678456134
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-03-27 22:23:06 +0000

    created validations for group and membership databases

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -21,4 +21,8 @@
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :group, counter_cache: true
+  
+  validates :user_id, :group_id, presence: true
+  validates :user_id, uniqueness: { scope: :group_id, message: "is already a member of this group" }
+
 end
