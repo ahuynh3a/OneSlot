0119d5ff23ab68ddf5d2f9c6d10bd38c45fdd864
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-03-27 22:02:01 +0000

    added validations for event database

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,4 +23,21 @@
 #
 class Event < ApplicationRecord
   belongs_to :calendar
+
+  validates :title, :start_date_time, :end_date_time, :calendar_id, :timezone, presence: true
+
+  validate :start_must_be_before_end
+
+  validates :timezone, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name),
+                                    message: "%{value} is not a valid timezone" }
+
+  private
+
+  def start_must_be_before_end
+    return if start_date_time.blank? || end_date_time.blank?
+
+    if start_date_time >= end_date_time
+      errors.add(:end_date_time, "must be after the start date and time")
+    end
+  end
 end
