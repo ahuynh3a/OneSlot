dcc25a081cb0f12e1703fe2fd011acea999a85c4
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-03-28 14:06:39 +0000

    made edits to type

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :calendars, foreign_key: "owner_id"
-  has_many :events, through: :calendarsI
+  has_many :events, through: :calendars
   has_many :memberships
   has_many :groups, through: :memberships
 
