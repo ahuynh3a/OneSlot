68cfc8173ef700e59de54ecb454a323af0f95e3a
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-04-01 00:17:49 +0000

    editeed sign up form so that people can sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   skip_forgery_protection
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :timezone])
+  end
 end
