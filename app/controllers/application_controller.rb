55ab36025629345fe01d61bf9a5d01835b9fc53c
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-04-04 20:24:19 +0000

    fixed sign up logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :email, :password, :password_confirmation, :current_password, :timezone])
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email, :password, :password_confirmation, :timezone])
+
   end
 
  
