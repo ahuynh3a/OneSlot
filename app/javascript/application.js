44854909c0197d6c90d4cc3a170ae6cb1d655569
Annie Huynh <150502695+ahuynh3a@users.noreply.github.com>
2024-04-05 21:42:40 +0000

    edited group calendar to showcase modal, event-indicators, and a current-day bubble

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.day').forEach(day => {
+    day.addEventListener('click', () => {
+      const modalId = day.getAttribute('data-target');
+      const modal = new bootstrap.Modal(document.querySelector(modalId));
+      modal.show();
+    });
+  });
+});
