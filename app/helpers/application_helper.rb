283a0361f36484ce5a74891b5585d6d5b58ec321
ahuynh3a <ahuynh3a@gmail.com>
2024-04-08 23:12:18 -0500

    created logic to show free times for each day in group calendar according to usertimezone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,33 @@ module ApplicationHelper
 
     [start_time_in_user_zone, end_time_in_user_zone]
   end
+
+
+  def available_times_for(date, events)
+    day_start = date.beginning_of_day
+    day_end = date.end_of_day
+
+    all_slots = (0...96).map { |i| day_start + (i * 15).minutes }.select { |time| time < day_end }
+
+    occupied_slots = events.flat_map do |event|
+      event_start = event.start_time
+      event_end = event.end_time
+
+      (event_start.to_i..event_end.to_i).step(15.minutes).map do |timestamp|
+        Time.zone.at(timestamp)
+      end
+    end
+
+    available_slots = all_slots.reject { |slot| occupied_slots.include?(slot) }
+
+    merge_consecutive_slots(available_slots)
+  end
+
+
+  def merge_consecutive_slots(slots)
+    merged = slots.slice_when { |i, j| j != i + 15.minutes }.to_a
+    merged.map do |range|
+      "#{range.first.strftime("%I:%M %p")} - #{(range.last + 15.minutes).strftime("%I:%M %p")}"
+    end
+  end
 end
